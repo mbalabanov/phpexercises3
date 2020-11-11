01b535c9c2dfbc557cb4f99f193d406ced52e1e4
Marin Balabanov <marincomics@gmail.com>
2020-11-11 09:48:52 +0100

    Added boilerplate and images

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+
+        <title>PHP Exercises Day 3</title>
+    </head>
+    <body class="bg-light">
+
+    <div class="container">
+        <h1 class="mt-4">PHP Exercises Day 3</h1>
+        <h2 class="mt-4">Restaurant Database</h2>
+        <div class="row mt-5">
+            <div class="d-inline-flex p-2 bd-highlight">I'm an inline flexbox container!</div>
+        </div>
+    </div>
+
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+
+  </body>
+</html>
+
+
+
+
+
